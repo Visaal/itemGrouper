7326a9676868275f0af8f2c8fdba896bc049e50b
Visaal Patel <visaalpatel@Visaals-MacBook-Pro.local>
2019-04-22 22:02:09 +0100

    add all item fields

--- js/itemGrouper.js
+++ js/itemGrouper.js
@@ -3,33 +3,33 @@
 class MasterItemList {
   constructor() {
     this.masterItemList = {};
+    this.masterItemAttributes = {};
     this.itemUlList = document.querySelector("#itemListArea ul");
   }
   createListItem(masterItem, masterList, masterItemSymbol) {
     let listItem = document.createElement("li");
-    let text = document.createTextNode(masterItem);
+    let tab = document.createTextNode("\t");
+    let itemNameText = document.createTextNode(masterItem["itemName"]);
+    let itemTypeText = document.createTextNode(masterItem["itemType"]);
     var attributesButton = document.createElement("button");
-    attributesButton.innerText = "Associate";
+    attributesButton.innerText = "delete";
     attributesButton.addEventListener("click", function() {
       // calling method from within another method - passed the masterList context into the button
       masterList.deleteMasterItem(listItem, masterItemSymbol);
     });
-    attributesButton.id = masterItem;
-    listItem.appendChild(text);
+    attributesButton.id = masterItem["itemName"];
+    listItem.appendChild(itemNameText);
+    listItem.appendChild(tab);
+    listItem.appendChild(itemTypeText);
     this.itemUlList.appendChild(listItem);
     listItem.appendChild(attributesButton);
   }
   addMasterItem(masterItem, masterList) {
-    if (masterItem) {
-      let masterItemSymbol = Symbol(masterItem);
+    if (masterItem["itemName"]) {
+      let masterItemSymbol = Symbol(masterItem["itemName"]);
       this.masterItemList[masterItemSymbol] = masterItem;
       this.createListItem(masterItem, masterList, masterItemSymbol);
     }
-  }
-  showMasterItems(htmlListAreaId) {
-    Object.keys(this.masterItemList).forEach(masterItem => {
-      this.createListItem(this.masterItemList[masterItem]);
-    });
   }
   deleteMasterItem(listItem, masterItemSymbol) {
     let ul = listItem.parentNode;
@@ -38,20 +38,88 @@ class MasterItemList {
   }
 }
 
-let masterList = new MasterItemList();
-
 let addItemButton = document.getElementById("addItemButton");
 addItemButton.addEventListener("click", function() {
   itemForm.classList.toggle("hide");
 });
 
+function createHtmlElementOnForm(
+  appendTo,
+  elementType,
+  type,
+  name,
+  id,
+  ...innerText
+) {
+  let newElement = document.createElement(elementType);
+  newElement.setAttribute("type", type);
+  newElement.setAttribute("name", name);
+  newElement.setAttribute("id", id);
+  if (innerText) {
+    newElement.innerText = innerText[0];
+  }
+  return appendTo.appendChild(newElement);
+}
+
+let masterList = new MasterItemList();
+
+let addAttributesButton = document.getElementById("addAttributesButton");
+addAttributesButton.addEventListener("click", function() {
+  let itemForm = document.getElementById("itemForm");
+
+  // Create fields to add attributes - fields only added once
+  if (!itemForm.elements["attributeName"]) {
+    // Create 'attribute name' field
+    let attributeName = createHtmlElementOnForm(
+      itemForm,
+      "input",
+      "text",
+      "attributeName",
+      "attributeName"
+    );
+
+    // Create 'attribute value' field
+    let attributeValue = createHtmlElementOnForm(
+      itemForm,
+      "input",
+      "text",
+      "attributeValue",
+      "attributeValue"
+    );
+
+    // Button to save the attibute value
+    let storeAttribute = createHtmlElementOnForm(
+      itemForm,
+      "button",
+      "button",
+      "storeAttributeButton",
+      "storeAttributeButton",
+      "Save Attribute"
+    );
+
+    storeAttribute.addEventListener("click", function() {
+      if (attributeName.value && attributeValue.value) {
+        masterList.masterItemAttributes[attributeName.value] =
+          attributeValue.value;
+        attributeName.value = null;
+        attributeValue.value = null;
+      }
+    });
+  }
+});
+
 let itemForm = document.getElementById("itemForm");
 itemForm.addEventListener("submit", event => {
-  let itemToAdd = itemForm.elements["itemName"].value;
+  let itemToAdd = {};
+  itemToAdd["itemType"] = itemForm.elements["itemType"].value;
+  itemToAdd["itemName"] = itemForm.elements["itemName"].value;
+  itemToAdd["startDate"] = itemForm.elements["startDate"].value;
+  itemToAdd["endDate"] = itemForm.elements["endDate"].value;
+  itemToAdd["attributes"] = masterList.masterItemAttributes;
+
   masterList.addMasterItem(itemToAdd, masterList);
   itemForm.reset();
+  masterList.masterItemAttributes = {};
   itemForm.classList.toggle("hide");
   event.preventDefault();
 });
-
-window.addEventListener("load", masterList.showMasterItems());
